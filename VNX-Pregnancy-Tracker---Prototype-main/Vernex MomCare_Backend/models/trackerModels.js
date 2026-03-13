28238fe0d67265c53c5b42dc943680b0a739a5cb
Abhinav <your-github-email@example.com>
2026-03-13 17:08:34 +0530

    remove zip

--- VNX-Pregnancy-Tracker---Prototype-main/Vernex MomCare_Backend/models/trackerModels.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  StepsTracker: require("./StepsTracker"),
-  HeartRateTracker: require("./HeartRateTracker"),
-  Spo2Tracker: require("./Spo2Tracker"),
-  SleepTracker: require("./SleepTracker"),
-  WaterTracker: require("./WaterTracker"),
-  WeightTracker: require("./WeightTracker"),
-  CaloriesTracker: require("./CaloriesTracker"),
-};
